1c9451eaca140959207268122eafce8f33bd29ef
searchernov <119491559+Sequr1@users.noreply.github.com>
2026-04-07 18:57:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,11 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
